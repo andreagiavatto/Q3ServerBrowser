b2f07c25e753348b08986628df0347185a120795
Andrea <hlr@HLR-MacBook-Pro.local>
2018-06-29 11:31:17 +0100

    updated q3 name strip

--- SQL/Utilities/String+Q3Name.swift
+++ SQL/Utilities/String+Q3Name.swift
@@ -19,9 +19,11 @@ public extension String {
         
         do {
             let regex = try NSRegularExpression(pattern: "\\^+[0-9]", options: .caseInsensitive)
-            decodedString = regex.stringByReplacingMatches(in: self, options: [], range: NSMakeRange(0, self.count), withTemplate: "")
+            decodedString = regex.stringByReplacingMatches(in: self, options: [], range: NSMakeRange(0, self.count), withTemplate: "") // removes simple colors
             let regex2 = try NSRegularExpression(pattern: "\\^+[0-9A-Z]{6}", options: .caseInsensitive)
-            decodedString = regex2.stringByReplacingMatches(in: decodedString, options: [], range: NSMakeRange(0, self.count), withTemplate: "")
+            decodedString = regex2.stringByReplacingMatches(in: decodedString, options: [], range: NSMakeRange(0, decodedString.count), withTemplate: "") // removes background colors
+            let regex3 = try NSRegularExpression(pattern: "\\^+[a-z]", options: .caseInsensitive)
+            decodedString = regex3.stringByReplacingMatches(in: decodedString, options: [], range: NSMakeRange(0, decodedString.count), withTemplate: "") // removes blinks and such
         } catch (let error) {
             print(error)
         }
